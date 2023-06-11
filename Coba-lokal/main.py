f575e298a775650b35fe4cf403000f5e16db9d94
Naufal_Rozan <naufalrozan22@gmail.com>
2023-06-11 19:17:49 +0700

    update input model

--- Coba-lokal/main.py
+++ Coba-lokal/main.py
@@ -5,29 +5,31 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from flask import Flask, request, jsonify
+import random
 
 model = keras.models.load_model("best_model.h5")
 
-def prepare_data(id_num, tipe_kendaraan, jenis_kendaraan, business):
+def prepare_data(tipe_kendaraan, jenis_kendaraan, business):
     tipe_kendaraan_dict = {
         'Mobil': 0,
         'Motor': 1
     }
     jenis_kendaraan_dict = {
-        'suv': 5,
+        'bebek': 0,
+        'hatchback': 1,
         'matic': 2,
         'mpv': 3,
-        'hatchback': 1,
-        'van': 6,
-        'bebek': 0,
-        'sedan': 4
+        'sedan': 4,
+        'suv': 5,
+        'van': 6
     }
     business_dict = {
-        'Liburan/Weekend': 1,
         'Daily Activity': 0,
+        'Liburan/Weekend': 1,
         'Travel': 2
     }
 
+    id_num = random.randint(1,1000)
     tipe_kendaraan_ = tipe_kendaraan_dict.get(tipe_kendaraan)
     jenis_kendaraan_ = jenis_kendaraan_dict.get(jenis_kendaraan)
     business_ = business_dict.get(business)
@@ -53,11 +55,11 @@ def index():
             if not input_data:
                 return jsonify({"error": "no data"})
 
-            feature1 = input_data.get('feature1')
+            # feature1 = input_data.get('feature1')
             feature2 = input_data.get('feature2')
             feature3 = input_data.get('feature3')
             feature4 = input_data.get('feature4')
-            features = prepare_data(feature1, feature2, feature3, feature4)
+            features = prepare_data(feature2, feature3, feature4)
             prediction = predict(features)
 
             data = {"prediction": prediction}
--- Model-Deployment/main.py
+++ Model-Deployment/main.py
@@ -5,29 +5,31 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from flask import Flask, request, jsonify
+import random
 
 model = keras.models.load_model("best_model.h5")
 
-def prepare_data(id_num, tipe_kendaraan, jenis_kendaraan, business):
+def prepare_data(tipe_kendaraan, jenis_kendaraan, business):
     tipe_kendaraan_dict = {
         'Mobil': 0,
         'Motor': 1
     }
     jenis_kendaraan_dict = {
-        'suv': 5,
+        'bebek': 0,
+        'hatchback': 1,
         'matic': 2,
         'mpv': 3,
-        'hatchback': 1,
-        'van': 6,
-        'bebek': 0,
-        'sedan': 4
+        'sedan': 4,
+        'suv': 5,
+        'van': 6
     }
     business_dict = {
-        'Liburan/Weekend': 1,
         'Daily Activity': 0,
+        'Liburan/Weekend': 1,
         'Travel': 2
     }
 
+    id_num = random.randint(1,1000)
     tipe_kendaraan_ = tipe_kendaraan_dict.get(tipe_kendaraan)
     jenis_kendaraan_ = jenis_kendaraan_dict.get(jenis_kendaraan)
     business_ = business_dict.get(business)
@@ -36,6 +38,7 @@ def prepare_data(id_num, tipe_kendaraan, jenis_kendaraan, business):
     return features
 
 def predict(x):
+    x = x.astype(float)  # Mengonversi x menjadi tipe data float
     predictions = model.predict(np.expand_dims(x, axis=0))
     labels = ['< 100.000', '100.000 - 200.000', '200.000 - 500.000', '> 500.000']
     label0 = np.argmax(predictions)
@@ -52,11 +55,11 @@ def index():
             if not input_data:
                 return jsonify({"error": "no data"})
 
-            feature1 = input_data.get('feature1')
+            # feature1 = input_data.get('feature1')
             feature2 = input_data.get('feature2')
             feature3 = input_data.get('feature3')
             feature4 = input_data.get('feature4')
-            features = prepare_data(feature1, feature2, feature3, feature4)
+            features = prepare_data(feature2, feature3, feature4)
             prediction = predict(features)
 
             data = {"prediction": prediction}
